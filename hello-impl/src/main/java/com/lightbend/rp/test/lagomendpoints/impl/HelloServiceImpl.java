cd796fee4f7478bafe937a995c5da03a006037e7
ilewis <idlewis@users.noreply.github.com>
2018-10-01 10:13:36 +0100

    Add some dummy root content for microclimate
    Microclimate appears to need '/' to return something valid
    so add some dummy content

--- hello-impl/src/main/java/com/lightbend/rp/test/lagomendpoints/impl/HelloServiceImpl.java
+++ hello-impl/src/main/java/com/lightbend/rp/test/lagomendpoints/impl/HelloServiceImpl.java
@@ -7,8 +7,16 @@ import akka.NotUsed;
 import java.util.concurrent.CompletableFuture;
 
 public class HelloServiceImpl implements HelloService {
+
+    private static final String ROOT_CONTENT = "Dummy root content. See http://localhost:9000/api/hello/world for the sample microservice\n";
+
     @Override
     public ServiceCall<NotUsed, String> hello(String id) {
         return request -> CompletableFuture.completedFuture("hello: " + id);
+    }
+
+    @Override
+    public ServiceCall<NotUsed, String> root() {
+        return request -> CompletableFuture.completedFuture(ROOT_CONTENT);
     }
 }
