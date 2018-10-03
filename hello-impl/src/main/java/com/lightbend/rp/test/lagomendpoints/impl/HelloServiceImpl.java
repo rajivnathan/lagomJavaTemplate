1d453459c1ad352e810d3fae16806b26bdcbc53d
ilewis <idlewis@users.noreply.github.com>
2018-10-03 15:06:10 +0100

    Make requests to root redirect to the api endpoint

--- hello-impl/src/main/java/com/lightbend/rp/test/lagomendpoints/impl/HelloServiceImpl.java
+++ hello-impl/src/main/java/com/lightbend/rp/test/lagomendpoints/impl/HelloServiceImpl.java
@@ -1,14 +1,25 @@
 package com.lightbend.rp.test.lagomendpoints.impl;
 
 import com.lightbend.rp.test.lagomendpoints.api.HelloService;
+import com.lightbend.lagom.javadsl.server.HeaderServiceCall;
+import com.lightbend.lagom.javadsl.api.transport.MessageProtocol;
+import com.lightbend.lagom.javadsl.api.transport.ResponseHeader;
 
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import akka.NotUsed;
+import akka.japi.Pair;
 import java.util.concurrent.CompletableFuture;
+import java.util.Optional;
+
+import org.pcollections.HashPMap;
+import org.pcollections.HashTreePMap;
+import org.pcollections.PSequence;
 
 public class HelloServiceImpl implements HelloService {
 
-    private static final String ROOT_CONTENT = "Dummy root content. See http://localhost:9000/api/hello/world for the sample microservice\n";
+    private static final String ROOT_CONTENT = "<html><head>" +
+                                               "<meta http-equiv=\"refresh\" content=\"0; url=/api/hello/world\">" +
+                                               "</head></html>";
 
     @Override
     public ServiceCall<NotUsed, String> hello(String id) {
@@ -16,7 +27,13 @@ public class HelloServiceImpl implements HelloService {
     }
 
     @Override
-    public ServiceCall<NotUsed, String> root() {
-        return request -> CompletableFuture.completedFuture(ROOT_CONTENT);
+    public HeaderServiceCall<NotUsed, String> root() {
+        return (reqHeaders, request) -> {
+            String responseBody = ROOT_CONTENT;
+            MessageProtocol proto = new MessageProtocol(Optional.of("text/html"), Optional.empty(), Optional.empty());
+            HashPMap<String, PSequence<String>> resHeaderMap = HashTreePMap.<String, PSequence<String>>empty();
+            ResponseHeader resHeaders = new ResponseHeader(200, proto, resHeaderMap);
+            return CompletableFuture.completedFuture(Pair.create(resHeaders, responseBody));
+        };
     }
 }
